b66ac297c18346b947ab80e1cf3873d9a9b395c9
mneifar-stg <mneifar-stg@spark-it.fr>
2018-11-18 11:15:53 +0100

    commit for backend folder

---
+++ back-end/src/main/java/tn/enis/EmspApplication.java
@@ -0,0 +1,12 @@
+package tn.enis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmspApplication.class, args);
+	}
+}
